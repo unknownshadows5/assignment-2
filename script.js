9cf9dc8eaf0882dfb7c7efb4522b478fc89a19a6
james johnson <59946406+unknownshadows5@users.noreply.github.com>
2020-01-25 14:01:25 -0800

    finished first 3 problems

--- script.js
+++ script.js
@@ -3,13 +3,16 @@
 
 // 1. Write code to output the following string to the console:
 // "Hello World!"
+console.log("Hello World!");
 
 // 2. Write code to concatenate three strings and output the result to the console:
 // "Hello", " ", "World!"
+console.log("Hello" + " " + "World!");
 
 // 3. Write code to output "Hello" on one line and "World!" on the next line to the console.
 // *Note: You should only have one console.log() statement - how do you add a new line in
 // a string?...
+console.log("Hello\nWorld!");
 
 // 4. Write code to output the result of the arithmetic expression to the console:
 // 3 * 8 + 2;
